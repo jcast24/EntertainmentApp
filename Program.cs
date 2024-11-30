7c21a96aecd35e5f747be626ab7133ea7f9c4354
Jesus Castro <castrojesus39@gmail.com>
2024-11-29 21:15:07 -0800

    initial creation of project with C#

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Spectre.Console;
+
+namespace MovieApp;
+
+class Program
+{
+    internal static void Main(string[] args)
+    {
+        var moviesList = new List<string>()
+        {
+            "Star Wars: A New Hope", 
+            "The Day After Tomorrow", 
+            "Man on Fire", 
+            "Avatar", 
+            "Children of Men",
+        };
+    }
+}
