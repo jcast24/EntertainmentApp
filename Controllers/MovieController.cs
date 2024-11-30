754cef719e8e476011d30ed360b4b9134d60aa94
Jesus Castro <castrojesus39@gmail.com>
2024-11-29 22:19:26 -0800

    movie controller + controller file created and implemented view movie method

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -1,6 +1,25 @@
-﻿namespace MovieApp.Controllers;
+﻿using Spectre.Console;
+
+namespace MovieApp.Controllers;
 
 public class MovieController
 {
-    
+    private static List<string> moviesList = new()
+    {
+        "Star Wars: A New Hope",
+        "The Day After Tomorrow",
+        "Man on Fire",
+        "Avatar",
+        "Children of Men",
+    };
+    internal static void ViewMovies()
+    {
+        AnsiConsole.MarkupLine("[yellow]List of movies: [/]");
+        foreach (var movie in moviesList)
+        {
+            AnsiConsole.MarkupLine($"- [cyan]{movie}[/]");
+        }
+        AnsiConsole.MarkupLine("Press any key to continue");
+        Console.ReadKey();
+    }
 }
