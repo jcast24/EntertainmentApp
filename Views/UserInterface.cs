71f072e3e406393699ff3b9c580b78dbbad5f14a
Jesus Castro <castrojesus39@gmail.com>
2024-11-29 22:18:54 -0800

    created views folder + added UserInterface.cs file, contains menu stuff

---
+++ Views/UserInterface.cs
@@ -0,0 +1,48 @@
+﻿using MovieApp.Controllers;
+using Spectre.Console;
+
+namespace MovieApp.Views;
+
+public class UserInterface
+{    
+    enum MenuChoices
+     {
+         AddMovie,
+         ViewMovies,
+         DeleteMovies,
+         ExitProg,
+     }
+    internal void MainMenu()
+    {
+        bool keepRunning = true;
+
+        while (keepRunning)
+        {
+            var choice = AnsiConsole.Prompt(
+                new SelectionPrompt<MenuChoices>()
+                    .Title("[cyan]Choose your choice:[/]")
+                    .AddChoices(Enum.GetValues<MenuChoices>()));
+
+            switch (choice)
+            {
+                case MenuChoices.AddMovie:
+                    // add movie
+                    break;
+                case MenuChoices.ViewMovies:
+                    // view movie
+                    MovieController.ViewMovies();
+                    break;
+                case MenuChoices.DeleteMovies:
+                    // delete movie
+                    break;
+                case MenuChoices.ExitProg:
+                    AnsiConsole.MarkupLine("[red]Goodbye![/]");
+                    keepRunning = false;
+                    break;
+                default:
+                    AnsiConsole.MarkupLine("Choose an option!");
+                    break;
+            }
+        } 
+    }
+}
