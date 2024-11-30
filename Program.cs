2ddf5b9e6e27ad1c66430973aa222b262f4d7899
Jesus Castro <castrojesus39@gmail.com>
2024-11-29 22:20:00 -0800

    Removed a bunch of stuff from Program.cs and moved into UserInterface and Movie Controller file

--- Program.cs
+++ Program.cs
@@ -1,56 +1,14 @@
-﻿using Spectre.Console;
+﻿using MovieApp.Controllers;
+using MovieApp.Views;
+using Spectre.Console;
 
 namespace MovieApp;
 
 class Program
 {
-    enum MenuChoices
-    {
-        AddMovie,
-        ViewMovies,
-        DeleteMovies,
-        ExitProg,
-    }
     internal static void Main(string[] args)
     {
-        bool keepRunning = true;
-        var moviesList = new List<string>()
-        {
-            "Star Wars: A New Hope", 
-            "The Day After Tomorrow", 
-            "Man on Fire", 
-            "Avatar", 
-            "Children of Men",
-        };
-
-        
-        
-        while (keepRunning)
-        {
-            var choice = AnsiConsole.Prompt(
-                new SelectionPrompt<MenuChoices>()
-                    .Title("Choose your choice:")
-                    .AddChoices(Enum.GetValues<MenuChoices>()));
-
-            switch (choice)
-            {
-                case MenuChoices.AddMovie:
-                    // add movie
-                    break;
-                case MenuChoices.ViewMovies:
-                    // view movie
-                    break;
-                case MenuChoices.DeleteMovies:
-                    // delete movie
-                    break;
-                case MenuChoices.ExitProg:
-                    AnsiConsole.MarkupLine("[red]Goodbye![/]");
-                    keepRunning = false;
-                    break;
-                default:
-                    AnsiConsole.MarkupLine("Choose an option!");
-                    break;
-            }
-        }
+        UserInterface menu = new UserInterface();
+        menu.MainMenu();
     }
 }
