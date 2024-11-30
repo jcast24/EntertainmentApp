3a6a1c3d3c20f6047af9d329b7977e5ea334dba5
Jesus Castro <castrojesus39@gmail.com>
2024-11-29 21:31:20 -0800

    basic menu with Spectre.Console added

--- Program.cs
+++ Program.cs
@@ -4,8 +4,16 @@ namespace MovieApp;
 
 class Program
 {
+    enum MenuChoices
+    {
+        AddMovie,
+        ViewMovies,
+        DeleteMovies,
+        ExitProg,
+    }
     internal static void Main(string[] args)
     {
+        bool keepRunning = true;
         var moviesList = new List<string>()
         {
             "Star Wars: A New Hope", 
@@ -14,5 +22,35 @@ class Program
             "Avatar", 
             "Children of Men",
         };
+
+        
+        
+        while (keepRunning)
+        {
+            var choice = AnsiConsole.Prompt(
+                new SelectionPrompt<MenuChoices>()
+                    .Title("Choose your choice:")
+                    .AddChoices(Enum.GetValues<MenuChoices>()));
+
+            switch (choice)
+            {
+                case MenuChoices.AddMovie:
+                    // add movie
+                    break;
+                case MenuChoices.ViewMovies:
+                    // view movie
+                    break;
+                case MenuChoices.DeleteMovies:
+                    // delete movie
+                    break;
+                case MenuChoices.ExitProg:
+                    AnsiConsole.MarkupLine("[red]Goodbye![/]");
+                    keepRunning = false;
+                    break;
+                default:
+                    AnsiConsole.MarkupLine("Choose an option!");
+                    break;
+            }
+        }
     }
 }
