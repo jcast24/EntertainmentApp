822adea101edde09d986364360dbc6e1033e4f34
Jesus Castro <castrojesus39@gmail.com>
2024-11-30 19:32:56 -0800

    method for deleting movie has been added

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -22,4 +22,43 @@ public class MovieController
         AnsiConsole.MarkupLine("Press any key to continue");
         Console.ReadKey();
     }
+
+    internal static void AddMovie()
+    {
+        var movieTitle = AnsiConsole.Ask<string>("Enter the name of the movie you would like to add: ");
+        
+        if (moviesList.Contains(movieTitle))
+        {
+            AnsiConsole.MarkupLine("[red]Movie is already in the list![/]");
+        }
+        else
+        {
+            moviesList.Add(movieTitle);
+            AnsiConsole.MarkupLine($"[green]{movieTitle} has been successfully added![/]");
+        }
+        
+        AnsiConsole.MarkupLine("Press any key to continue!");
+        Console.ReadKey();
+    }
+
+    internal static void DeleteMovie()
+    {
+        if (moviesList.Count == 0)
+        {
+            AnsiConsole.MarkupLine("[red] No books have been found! [/]");
+        }
+        
+        var movieTitle = AnsiConsole.Ask<string>("Enter the name of the movie that you would like to delete: ");
+
+        if (moviesList.Remove(movieTitle))
+        {
+            AnsiConsole.MarkupLine($"[green] {movieTitle} has been successfully deleted. [/]");
+        }
+        else
+        {
+            AnsiConsole.MarkupLine($"[red]Book not found[/]");
+        }
+        AnsiConsole.MarkupLine("Press any key to continue!");
+        Console.ReadKey();
+    }
 }
