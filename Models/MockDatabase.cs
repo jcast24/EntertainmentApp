052fd6d74be0374e2cfe18fae43e297dcc10bb86
Jesus Castro <castrojesus39@gmail.com>
2024-12-03 16:39:18 -0800

    models directory created with MockDB and added List for movies

---
+++ Models/MockDatabase.cs
@@ -0,0 +1,13 @@
+﻿namespace MovieApp.Models;
+
+public class MockDatabase
+{
+   internal static List<string> Movies = new List<string>()
+   {
+        "Star Wars: A New Hope",
+        "The Day After Tomorrow",
+        "Man on Fire",
+        "Avatar",
+        "Children of Men",
+   };
+}
