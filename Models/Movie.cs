5e4a58e4acadee319817de9595dc5ebaa49e40b7
Jesus Castro <castrojesus39@gmail.com>
2024-12-03 17:43:03 -0800

    created a Movie model file to keep track of title, genre, and score

---
+++ Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿namespace MovieApp.Models;
+
+internal class Movie
+{
+    public string Title { get; set; }
+    public string Genre { get; set; } 
+    public int Score { get; set; } 
+
+    public Movie(string title, string genre, int score)
+    {
+        Title = title;
+        Genre = genre;
+        Score = score;
+    }
+
+}
