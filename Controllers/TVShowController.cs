f49e2877adf3f0c9ef26a35414b2be2acdf12671
Jesus Castro <castrojesus39@gmail.com>
2025-01-05 23:51:02 -0800

    Created structure/added methods for tv show controller, still need to add method implementation

---
+++ Controllers/TVShowController.cs
@@ -0,0 +1,20 @@
+using MovieApp.Models;
+using Spectre.Console;
+
+namespace MovieApp.Controllers;
+
+public class TVShowController : IBaseController 
+{
+    public void ViewItems() {
+        throw new NotImplementedException();
+    }
+
+    public void AddItem() {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteItem() 
+    {
+        throw new NotImplementedException();
+    }
+}
