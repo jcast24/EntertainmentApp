238f55bc54cb5774f3e4de7ced1a9f13905a2df3
Jesus Castro <castrojesus39@gmail.com>
2025-01-06 23:21:04 -0800

    ViewItems method created for TVShow controller

--- Controllers/TVShowController.cs
+++ Controllers/TVShowController.cs
@@ -3,17 +3,38 @@ using Spectre.Console;
 
 namespace MovieApp.Controllers;
 
-public class TVShowController : IBaseController 
+public class TVShowController : IBaseController
 {
-    public void ViewItems() {
+    public void ViewItems()
+    {
+        var table = new Table();
+
+        table.Border(TableBorder.Rounded);
+
+        table.AddColumn("[cyan]Title[/]");
+        table.AddColumn("[cyan]Genre[/]");
+        table.AddColumn("[cyan]Score[/]");
+
+        foreach (var tvShow in MockDatabase.TV)
+        {
+            table.AddRow(
+                $"[cyan]{tvShow.Title}[/]",
+                $"[cyan]{tvShow.Genre}[/]",
+                $"[cyan]{tvShow.Score}[/]"
+            );
+        }
+
+        AnsiConsole.Write(table);
+        AnsiConsole.MarkupLine("Press any key to continue");
+        Console.ReadKey();
+    }
+
+    public void AddItem()
+    {
         throw new NotImplementedException();
     }
 
-    public void AddItem() {
-        throw new NotImplementedException();
-    }
-
-    public void DeleteItem() 
+    public void DeleteItem()
     {
         throw new NotImplementedException();
     }
